88bac64254fe2a707bc2784d6ec689110f84dee7
ranghel <anghelramona30@gmail.com>
2017-04-28 12:36:20 -0400

    created directories and files

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES("Big Mac", TRUE, CURRENT_TIMESTAMP );
+
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES("Bacon Cheese Burger", FALSE , CURRENT_TIMESTAMP );
+
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES("World's Most Famos Burger", TRUE , CURRENT_TIMESTAMP );
