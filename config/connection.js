12fe7192061b35e95fe7b0441b1bc17d67487bdd
ranghel <anghelramona30@gmail.com>
2017-05-07 16:41:19 -0400

    deleted index.php and composer.json

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     connection = mysql.createConnection({
-        root: 3000,
+        root: 3306,
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'burgers_db',
+        database: 'burgers_db'
     });
 };
 
